31f7e3c3caa9567f3db4d2c64f923e45943a2ecc
Anu <anu04223@gmail.com>
2024-03-30 02:20:24 +0530

    modified vue config 2

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,5 @@
 // })
 
 module.exports={
-  publicPath: process.env.NODE_ENV ==='production'
-  ?'/self-protal/'
-  :'/',
+  publicPath: process.env.NODE_ENV ==='production'?'/SelfPortal/':'/',
 };
